cd561ebc5245555f2cac9c855fc8bcd7eaaaec43
Bochkov Anton <bochkov.dev@yandex.ru>
2024-09-25 12:52:56 +0300

    Rewrite tests.

--- Python/tests/test_singly_linked_list.py
+++ Python/tests/test_singly_linked_list.py
@@ -1,108 +1,150 @@
+import pytest
 from singly_linked_list import SinglyLinkedList
 
-def test_append():
-    linked_list = SinglyLinkedList()
-    linked_list.append(1)
-    linked_list.append(2)
-    assert linked_list.head.data == 1
-    assert linked_list.head.next.data == 2
-    assert len(linked_list) == 2
 
-def test_prepend():
-    linked_list = SinglyLinkedList()
-    linked_list.prepend(2)
-    linked_list.prepend(1)
-    assert linked_list.head.data == 1
-    assert linked_list.head.next.data == 2
-    assert len(linked_list) == 2
+@pytest.fixture
+def empty_list():
+    return SinglyLinkedList()
 
-def test_insert():
-    linked_list = SinglyLinkedList()
-    linked_list.append(1)
-    linked_list.append(3)
-    linked_list.insert(1, 2)
-    assert linked_list[0] == 1
-    assert linked_list[1] == 2
-    assert linked_list[2] == 3
 
-def test_extend():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert linked_list[0] == 1
-    assert linked_list[1] == 2
-    assert linked_list[2] == 3
-    assert len(linked_list) == 3
+@pytest.fixture
+def filled_list():
+    lst = SinglyLinkedList()
+    lst.extend([1, 2, 3])
+    return lst
 
-def test_delete():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    linked_list.delete(2)
-    assert linked_list[0] == 1
-    assert linked_list[1] == 3
-    assert len(linked_list) == 2
 
-def test_delete_at():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    linked_list.delete_at(1)
-    assert linked_list[0] == 1
-    assert linked_list[1] == 3
-    assert len(linked_list) == 2
+@pytest.mark.parametrize("values,expected", [
+    ([1], [1]),
+    ([1, 2, 3], [1, 2, 3]),
+    ([], []),
+])
+def test_append(empty_list, values, expected):
+    for value in values:
+        empty_list.append(value)
+    assert list(empty_list) == expected
 
-def test_remove_duplicates():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 2, 3])
-    linked_list.remove_duplicates()
-    assert linked_list[0] == 1
-    assert linked_list[1] == 2
-    assert linked_list[2] == 3
-    assert len(linked_list) == 3
 
-def test_clear():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    linked_list.clear()
-    assert len(linked_list) == 0
-    assert linked_list.is_empty()
+@pytest.mark.parametrize("values,expected", [
+    ([1], [1]),
+    ([1, 2, 3], [3, 2, 1]),
+    ([], []),
+])
+def test_prepend(empty_list, values, expected):
+    for value in values:
+        empty_list.prepend(value)
+    assert list(empty_list) == expected
 
-def test_find():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert linked_list.find(2) == 1
-    assert linked_list.find(4) is None
 
-def test_reverse():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    linked_list.reverse()
-    assert linked_list[0] == 3
-    assert linked_list[1] == 2
-    assert linked_list[2] == 1
+@pytest.mark.parametrize("index, value, expected", [
+    (0, 1, [1, 1, 2, 3]),
+    (1, 4, [1, 4, 2, 3]),
+    (2, 5, [1, 2, 5, 3]),
+])
+def test_insert(filled_list, index, value, expected):
+    filled_list.insert(index, value)
+    assert list(filled_list) == expected
 
-def test_is_empty():
-    linked_list = SinglyLinkedList()
-    assert linked_list.is_empty()
-    linked_list.append(1)
-    assert not linked_list.is_empty()
 
-def test_get_set_item():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert linked_list[1] == 2
-    linked_list[1] = 4
-    assert linked_list[1] == 4
+@pytest.mark.parametrize("value, expected", [
+    (1, [2, 3]),
+    (2, [1, 3]),
+    (3, [1, 2]),
+    (4, [1, 2, 3]),
+])
+def test_delete(filled_list, value, expected):
+    filled_list.delete(value)
+    assert list(filled_list) == expected
 
-def test_del_item():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    del linked_list[1]
-    assert linked_list[0] == 1
-    assert linked_list[1] == 3
 
-def test_len():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert len(linked_list) == 3
+@pytest.mark.parametrize("index, expected", [
+    (0, [2, 3]),
+    (1, [1, 3]),
+    (2, [1, 2]),
+    (3, [1, 2, 3]),  
+])
+def test_delete_at(filled_list, index, expected):
+    try:
+        filled_list.delete_at(index)
+    except IndexError:
+        assert list(filled_list) == expected
+    else:
+        assert list(filled_list) == expected
+
+
+@pytest.mark.parametrize("values,expected", [
+    ([4, 5], [1, 2, 3, 4, 5]),
+    ([6], [1, 2, 3, 6]),
+    ([], [1, 2, 3]),
+])
+def test_extend(filled_list, values, expected):
+    filled_list.extend(values)
+    assert list(filled_list) == expected
+
+
+@pytest.mark.parametrize("initial_values,expected", [
+    ([1, 1, 2, 2, 3, 3], [1, 2, 3]),
+    ([1, 2, 3], [1, 2, 3]),
+    ([1, 1, 1], [1]),
+])
+def test_remove_duplicates(initial_values, expected):
+    lst = SinglyLinkedList()
+    lst.extend(initial_values)
+    lst.remove_duplicates()
+    assert list(lst) == expected
+
+
+@pytest.mark.parametrize("initial_values,expected", [
+    ([1, 2, 3], [3, 2, 1]),
+    ([1], [1]),
+    ([], []),
+])
+def test_reverse(initial_values, expected):
+    lst = SinglyLinkedList()
+    lst.extend(initial_values)
+    lst.reverse()
+    assert list(lst) == expected
+
+
+@pytest.mark.parametrize("value, expected", [
+    (1, 0),
+    (2, 1),
+    (3, 2),
+    (4, None),
+])
+def test_find(filled_list, value, expected):
+    assert filled_list.find(value) == expected
+
+
+def test_get_set_item(filled_list):
+    assert filled_list[1] == 2
+    filled_list[1] = 4
+    assert filled_list[1] == 4
+
+
+def test_del_item(filled_list):
+    del filled_list[1]
+    assert list(filled_list) == [1, 3]
+
+
+def test_add(filled_list):
+    list1 = SinglyLinkedList()
+    list1.extend([4, 5])
+    new_list = filled_list + list1
+    assert list(new_list) == [1, 2, 3, 4, 5]
+
+
+@pytest.mark.parametrize("value, expected", [
+    (2, True),
+    (4, False),
+])
+def test_contains(filled_list, value, expected):
+    assert (value in filled_list) == expected
+
+
+def test_len(filled_list):
+    assert len(filled_list) == 3
+
 
 def test_eq():
     list1 = SinglyLinkedList()
@@ -114,37 +156,7 @@ def test_eq():
     list2.append(4)
     assert list1 != list2
 
-def test_add():
-    list1 = SinglyLinkedList()
-    list2 = SinglyLinkedList()
-    list1.extend([1, 2])
-    list2.extend([3, 4])
-    new_list = list1 + list2
-    print(list1, list2)
-    print(new_list)
-    assert new_list[0] == 1
-    assert new_list[1] == 2
-    assert new_list[2] == 3
-    assert new_list[3] == 4
 
-def test_contains():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert 2 in linked_list
-    assert 4 not in linked_list
-
-def test_iter():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert list(iter(linked_list)) == [1, 2, 3]
-
-def test_reversed():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert list(reversed(linked_list)) == [3, 2, 1]
-
-def test_str_repr():
-    linked_list = SinglyLinkedList()
-    linked_list.extend([1, 2, 3])
-    assert str(linked_list) == '[1, 2, 3]'
-    assert repr(linked_list) == '[1, 2, 3]'
+def test_iter_reversed(filled_list):
+    assert list(iter(filled_list)) == [1, 2, 3]
+    assert list(reversed(filled_list)) == [3, 2, 1]
